011179e56de49cda70deabace27e6171023c652a
Ruben Dedoncker <ruben_dedoncker@hotmail.com>
2016-02-23 12:39:46 +0100

    Finale test

--- src/hillbillies/model/Unit.java
+++ src/hillbillies/model/Unit.java
@@ -53,7 +53,7 @@ public class Unit {
 	
 	private static int NAMELENGTH_MIN = 2;
 	
-	private static Set<String> VALIDCHARS = new HashSet<String>(Arrays.asList(" ", "\"", "\'", "m"));
+	private static Set<String> VALIDCHARS = new HashSet<String>(Arrays.asList(" ", "\"", "\'"));
 	
 	
 }
